f7e028c872466051f3a8278d5c8391d4e937660a
El Werbitzky <werbitzky@gmail.com>
2015-07-13 22:35:30 +0200

    define Elastix.start

--- lib/elastix.ex
+++ lib/elastix.ex
@@ -7,6 +7,11 @@ defmodule Elastix do
   if !Dict.get(Application.get_env(:elastix, Elastix), :elastic_url), do: raise "Elastix requires an :elastic_url"
   
   @doc false
+  def start do
+    :application.ensure_all_started(:elastix)
+  end
+
+  @doc false
   def config, do: Application.get_env(:elastix, Elastix)
   @doc false
   def config(key), do: Dict.get(config, key)
