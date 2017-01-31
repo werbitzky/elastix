34a86c12e88fb8d9d328a89ae256fb5b4d07a801
Kevin Rockwood <kevin@rockwood.me>
2017-01-31 10:39:10 +0900

    Upgrade ex_doc to 0.14.5
    Earmark is the default markdown parser it's not nesessary to depend
    on it.

--- mix.exs
+++ mix.exs
@@ -32,8 +32,7 @@ defmodule Elastix.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.7", only: :dev},
+    [{:ex_doc, "~> 0.14", only: :dev},
      {:credo, "~> 0.6", only: [:dev, :test]},
      {:mix_test_watch, "~> 0.3", only: [:test, :dev]},
      {:poison, "~> 1.5 or ~> 2.0"},
