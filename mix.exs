153ce9a761276b0a2c49e2558c7ad773bfed176c
Kevin Rockwood <kevin@rockwood.me>
2017-01-31 10:53:39 +0900

    Upgrade poison to 3.1
    Poison no longer parses empty strings as valid json, so this adds adds
    an additional function head when responses are empty strings.

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Elastix.Mixfile do
     [{:ex_doc, "~> 0.14", only: :dev},
      {:credo, "~> 0.6", only: [:dev, :test]},
      {:mix_test_watch, "~> 0.3", only: [:test, :dev]},
-     {:poison, "~> 1.5 or ~> 2.0"},
+     {:poison, "~> 3.1"},
      {:httpoison, "~> 0.10"}]
   end
 
