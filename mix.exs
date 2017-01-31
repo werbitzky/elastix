6597675f2a22c3bd157d4d30d907eff37d687145
Kevin Rockwood <kevin@rockwood.me>
2017-01-31 11:06:13 +0900

    Upgrade httpoison to 0.11

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Elastix.Mixfile do
      {:credo, "~> 0.6", only: [:dev, :test]},
      {:mix_test_watch, "~> 0.3", only: [:test, :dev]},
      {:poison, "~> 3.1"},
-     {:httpoison, "~> 0.10"}]
+     {:httpoison, "~> 0.11"}]
   end
 
   defp package do
