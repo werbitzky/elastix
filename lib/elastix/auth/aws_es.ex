ed06a34f003a3dce42f5a49617180f8fdf528817
Adam Jacox <yellowjacox@gmail.com>
2017-03-25 18:46:10 -0600

    Add host and amz-content-sha-256 headers

--- lib/elastix/auth/aws_es.ex
+++ lib/elastix/auth/aws_es.ex
@@ -4,19 +4,23 @@ defmodule Elastix.Auth.AWSES do
 	https://github.com/bryanjos/aws_auth
 	"""
 
-	def process_headers(headers, request_data) do
+	def process_headers(headers, {_, url, body} = request_data) do
 		# cache current time for x-amz-date header and signing auth header
 		datetime = current_time()
 
 		headers
+    |> put_host_header(url)
 		|> put_amz_date_header(datetime)
+    |> put_amz_content_sha_256_header(body)
 		|> put_signed_request_header(request_data, datetime)
   end
 
   defp put_signed_request_header(headers, {method, url, body}, datetime) do
   	configs      = Elastix.config(:aws_es)
     method       = method |> Atom.to_string() |> String.upcase()
-    auth_headers = Enum.map(headers, fn {k,v} -> {Atom.to_string(k), v} end) |> Enum.into(%{})
+
+    # the aws_auth library requires headers as a map (vs. keyword list used by HTTPoison)
+    auth_headers = headers_to_map(headers)
 
     signed_request = AWSAuth.sign_authorization_header(
       configs[:access_key], 
@@ -33,12 +37,27 @@ defmodule Elastix.Auth.AWSES do
     Keyword.put(headers, :"Authorization", signed_request)
   end
 
+  defp put_host_header(headers, url) do
+    host = URI.parse(url).host
+    Keyword.put(headers, :"host", host)
+  end
+
   defp put_amz_date_header(headers, datetime) do
   	Keyword.put(headers, :"x-amz-date", AWSAuth.Utils.format_time(datetime))
+  end
+
+  defp put_amz_content_sha_256_header(headers, body) do
+    Keyword.put(headers, :"x-amz-content-sha256", AWSAuth.Utils.hash_sha256(body))
   end
 
   defp current_time do
   	DateTime.utc_now() 
   	|> DateTime.to_naive()
+  end
+
+  defp headers_to_map(headers) do
+    headers
+    |> Enum.map(fn {k,v} -> {Atom.to_string(k), v} end) 
+    |> Enum.into(%{})
   end
 end
