4e57be455396a5564c9a82b12152c2dba271acd2
El Werbitzky <werbitzky@gmail.com>
2021-03-20 02:43:53 +0100

    Fix race condition on alias index test.

--- test/elastix/alias_test.exs
+++ test/elastix/alias_test.exs
@@ -13,7 +13,7 @@ defmodule Elastix.AliasTest do
   end
 
   test "aliases actions on existing index should respond with 200" do
-    assert {:ok, %{status_code: 200}} = Index.create(@test_url, @test_index, %{})
+    assert {:ok, %{status_code: _}} = Index.create(@test_url, @test_index, %{})
 
     assert {:ok, %{status_code: 200}} =
              Alias.post(@test_url, [
@@ -23,7 +23,7 @@ defmodule Elastix.AliasTest do
   end
 
   test "remove unkown alias on existing index should respond with 404" do
-    assert {:ok, %{status_code: 200}} = Index.create(@test_url, @test_index, %{})
+    assert {:ok, %{status_code: _}} = Index.create(@test_url, @test_index, %{})
 
     assert {:ok, %{status_code: 404}} =
              Alias.post(@test_url, [%{remove: %{index: @test_index, alias: "alias1"}}])
